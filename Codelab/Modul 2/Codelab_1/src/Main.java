cdad8cba704c472454f6a28920f72de05bbf75f7
WAHYUDIN10 <wahyu49204@webmail.umm.ac.id>
2025-03-17 22:03:08 +0700

    moy

---
+++ Codelab/Modul 2/Codelab_1/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Hewan hewan1 = new Hewan(); //membuat object hewan1 dan hewan2
+        Hewan hewan2 = new Hewan();
+
+        hewan1.inputDataHewan(input); //memanggil method di class Hewan untuk meminta input dari user dari objek hewan1
+        System.out.println();
+        hewan2.inputDataHewan(input); //memanggil method di class Hewan untuk meminta input dari user dari objek hewan2
+
+        System.out.println("\n======== Informasi Hewan =======");
+        hewan1.tampilkanInfo(); // memanggil method di class hewan untuk menampilkan data dari hewan1
+        hewan2.tampilkanInfo();// memanggil method di class hewan untuk menampilkan data dari hewan2
+        input.close();
+    }
+}
